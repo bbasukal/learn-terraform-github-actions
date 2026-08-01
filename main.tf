6598cfc153fa38cb4e747031ed4a79928aada3c8
binaya basukala <43363861+bbasukal@users.noreply.github.com>
2026-08-01 16:58:29 -0700

    updated the main.tf

--- main.tf
+++ main.tf
@@ -15,10 +15,12 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "bbasukal"
 
     workspaces {
-      name = "gh-actions-demo"
+      project = "'Learn terraform'"
+      name    = "learn-terraform-github-actions"
+
     }
   }
 }
